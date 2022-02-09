7a745883e0d017ba1d104fc7e668239932d49f36
CarolynnFleming <cfcatpink@gmail.com>
2022-02-08 19:22:27 -0500

    get pokemon

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,1 +1,11 @@
 import { client, checkError } from './client';
+
+export async function getPokemon(start = 1, end = 20) {
+  const response = await client
+    .from('pokemon')
+    .select()
+    .range(start, end);
+
+  return checkError(response);
+}
+
