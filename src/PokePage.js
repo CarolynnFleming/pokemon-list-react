9c6d821632a3cc8ec777eecf150e3d6db87cad66
CarolynnFleming <cfcatpink@gmail.com>
2022-02-08 20:19:13 -0500

    useEffect

--- src/PokePage.js
+++ src/PokePage.js
@@ -5,6 +5,18 @@ import { getPokemon } from './services/fetch-utils';
 const PER_PAGE = 40;
 
 export default function PokePage() {
-    const [page, setPage] = useState(1);
-    const [pokemon, setpokemon] = useState([]);
+  const [page, setPage] = useState(1);
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    async function fetch() {
+      const start = PER_PAGE * (page - 1) + 1;
+      const end = PER_PAGE * page;
+
+      const data = await getPokemon(start, end);
+
+      setPokemon(data);
+    }
+    fetch();
+  }, [page]);
 }
