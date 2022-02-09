238a0ba75782ef1ba79628f3b0380ef9a509ab28
CarolynnFleming <cfcatpink@gmail.com>
2022-02-08 20:00:48 -0500

    pokepoke

--- src/PokePoke.js
+++ src/PokePoke.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function PokePoke() {
-  return <div></div>;
+export default function PokePoke({ pokemon }) {
+  return (
+    <Link to={`/detail/${pokemon.id}`}>
+      <div className='pokemon-mon'>
+        <p>{pokemon.Pokemon}</p>
+        <img className='pokemon-img' src={pokemon.GIF}/>
+      </div>
+    </Link>
+  );
 }
