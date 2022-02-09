bc4501df3091adda996a0a2e1a75a70f66fa9480
CarolynnFleming <cfcatpink@gmail.com>
2022-02-08 20:14:25 -0500

    state

--- src/PokePage.js
+++ src/PokePage.js
@@ -1,3 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getPokemon } from './services/fetch-utils';
+
+const PER_PAGE = 40;
+
+export default function PokePage() {
+    const [page, setPage] = useState(1);
+    const [pokemon, setpokemon] = useState([]);
+}
